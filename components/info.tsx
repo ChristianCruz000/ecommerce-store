bb8f365f16276e72856aefb3430ea2149cfa0a39
Christian <christian@simpleshape.me>
2023-10-18 09:37:17 -0400

    fixed onClick action

--- components/info.tsx
+++ components/info.tsx
@@ -1,12 +1,18 @@
 import { Product } from "@/types";
 import Currency from "@/components/ui/currency";
 import Button from "@/components/ui/button";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product;
 }
 
 const info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+
+  const onAddToCart = () => {
+    cart.addItem(data);
+  };
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -48,7 +54,9 @@ const info: React.FC<InfoProps> = ({ data }) => {
         Modificar Contenido
       </button>
       <br />
-      <Button className="flex items-center gap-x-2">Agregar canasta</Button>
+      <Button onClick={onAddToCart} className="flex items-center gap-x-2">
+        Agregar canasta
+      </Button>
     </div>
   );
 };
