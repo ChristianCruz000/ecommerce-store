45e92f3ed5fe1949086e2ba31f1068e30f2d5927
Christian <christian@simpleshape.me>
2023-10-18 08:58:55 -0400

    fixed Image error

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import MainNav from "./main-nav";
 import getCategories from "@/actions/get-categories";
 import NavbarActions from "./navbar-actions";
+import Image from "next/image";
 
 const Navbar = async () => {
   const categories = await getCategories();
@@ -11,7 +12,7 @@ const Navbar = async () => {
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <img src="/logo_boxes.svg" width="100" />
+            <Image alt="logo" src="/logo_boxes.svg" width="100" />
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
